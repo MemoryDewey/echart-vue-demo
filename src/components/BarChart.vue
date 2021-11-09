2b3cbc829f720011f47d85b30c2de8598232ca48
weidu <wei.du@shopee.com>
2021-11-09 16:41:03 +0800

    feat: init project

---
+++ src/components/BarChart.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="bar-chart" ref="chart"></div>
+</template>
+
+<script>
+import * as echarts from 'echarts'
+
+export default {
+  name: 'BarChart',
+  props: {
+    xData: {
+      type: Array,
+      default: () => []
+    },
+    yData: {
+      type: Array,
+      default: () => []
+    },
+    title: {
+      type: String,
+      default: '',
+    }
+  },
+  data() {
+    return {
+      chartInstance: null
+    }
+  },
+  methods: {
+    updateChart() {
+      this.chartInstance.setOption({
+        title: {
+          text: this.title,
+          left: 'center'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow'
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'category',
+            data: this.xData,
+            axisTick: {
+              alignWithLabel: true
+            }
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: 'Direct',
+            type: 'bar',
+            barWidth: 30,
+            data: this.yData,
+          }
+        ]
+      })
+    }
+  },
+  watch: {
+    xData() {
+      this.updateChart()
+    },
+    yData() {
+      this.updateChart()
+    }
+  },
+  mounted() {
+    const chartDom = this.$refs.chart
+    this.chartInstance = echarts.init(chartDom)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.bar-chart {
+  height: 300px;
+  width: 100%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,93 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="demo">
+    <div class="info item">
+      <div class="city">{{ this.city }}</div>
+      <div class="avg">Average Price: {{ this.avgPrice }}</div>
+    </div>
+    <bar-chart class="item" :x-data="roomXData" :y-data="roomYData" title="Room Type" />
+    <bar-chart class="item" :x-data="tenancyXData" :y-data="tenancyYData" title="Tenancy Term" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import BarChart from "@/components/BarChart";
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: { BarChart },
+  data() {
+    return {
+      roomXData: [],
+      roomYData: [],
+      tenancyXData: [],
+      tenancyYData: [],
+      city: '',
+      avgPrice: 0,
+    }
+  },
+  methods: {
+    // mock的数据
+    mockRes() {
+      return {
+        "city": "Toronto",
+        "room_type_x": [
+          "Entire home/apt",
+          "Private room"
+        ],
+        "room_type_y": [
+          9764,
+          4918
+        ],
+        "tenancy_term_x": [
+          "Long Rent",
+          "Short Rent"
+        ],
+        "tenancy_term_y": [
+          11057,
+          3901
+        ],
+        "Average_price": 159.80002674333087,
+        "msg": "success",
+        "error_num": 0
+      }
+    }
+  },
+  mounted() {
+    const res = this.mockRes()
+    this.city = res.city
+    this.avgPrice = res.Average_price.toFixed(2)
+    this.roomXData = res.room_type_x
+    this.roomYData = res.room_type_y
+    this.tenancyXData = res.tenancy_term_x
+    this.tenancyYData = res.tenancy_term_y
   }
 }
 </script>
+
+<style scoped>
+.demo {
+  display: flex;
+  border: 1px solid #2c3e50;
+}
+
+.item {
+  flex: 1;
+}
+
+.item:not(:last-child) {
+  border-right: 1px solid #2c3e50;
+}
+
+.info {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.city, .avg {
+  font-size: 20px;
+}
+</style>
